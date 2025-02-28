0091a643033c35603dcc951ca692ad369202a081
Elhashash <anwar3779432@gmail.com>
2025-02-28 13:53:21 +0200

    Start Dabiels Project

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+let nav = document.getElementById("nav"); // nav
+let link = document.querySelectorAll(".navbar-nav .nav-link"); // ul > li
+let navLogo = document.getElementById("navLogo"); // nav logo
+let navIcon = document.getElementById("navbar-toggler"); // nav icon
+window.onscroll = function () {
+  if (window.scrollY >= 550) {
+    nav.style.cssText = `
+    position: fixed;
+    background-color: #fff;
+    padding: 0 0 30px;
+    box-shadow: 0px 30px 50px rgba(0, 0, 0, 0.05);
+`;
+    navLogo.style.color = "#343434";
+    for (let i = 0; i < link.length; i++) {
+      link[i].style.color = "#111111";
+    }
+    navIcon.style.color = "#343434";
+  } else {
+    nav.style.cssText = `
+    position: absolute;
+    background-color: transparent;
+    padding: 20px 0 30px;
+    box-shadow: none;
+`;
+    navLogo.style.color = "#fff";
+    for (let i = 0; i < link.length; i++) {
+      link[i].style.color = "#fff";
+    }
+    navIcon.style.color = "#fff";
+  }
+};
+
+navIcon.onclick = function () {
+  navIcon.style.color = `#343434`;
+  nav.style.backgroundColor = "#fff";
+  navLogo.style.color = `#343434`;
+  for (let i = 0; i < link.length; i++) {
+    link[i].style.color = `#111111`;
+  }
+};
